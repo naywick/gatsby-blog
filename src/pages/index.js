33385aa50c47f4c78208047b788e893f6a561bd9
Nayela Wickramasuriya <nayelawickramasuriya@Nayelas-MBP.lan>
2021-09-19 12:01:36 +0100

    Update landing page intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ class IndexPage extends React.Component {
           I am a software developer working primarily in Ruby on Rails, though I also dabble in some JavaScript/TypeScript. I'm proud to work at <a href="https://www.patchwork.health/">Patchwork Health</a> helping the NHS.
         </p>
         <p>
-          I also write and was shortlisted for the <a href="https://bricklanebookshop.org/2021-short-story-prize/">Brick Lane Bookshop Short Story Prize</a>. When I am not writing code, I write regular words and am currently working on a novel about two women from my native Sri Lanka.
+          I also write and was shortlisted for the <a href="https://bricklanebookshop.org/2021-short-story-prize/">Brick Lane Bookshop Short Story Prize</a>. I am currently working on a novel about two women from my native Sri Lanka.
         </p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
